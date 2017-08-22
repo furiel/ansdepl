64c91ccb08efa31e91b69a802ec9810a940d2283
John Doe <johndoe@example.com>
2017-08-22 17:25:25 +0200

    Initial code

---
+++ ansdepl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+REPO_PATH=https://github.com/furiel/ansdepl.git
+TMP_FILES=/var/pull
+
+ansible-pull -o -v -U "$REPO_PATH" -d "$TMP_FILES" -i "localhost," -c local ansdepl.yml
+
+while [[ $RESULT -ne 0 ]] && [ $COUNTER -lt 10 ]; do
+  echo "+++Could not pull | $COUNTER. Try"
+  COUNTER=$[COUNTER + 1]
+  sleep 10
+
+  ansible-pull -p -U "$REPO_PATH" -d "$TMP_FILES" -i "localhost," -c local ansdepl.yml
+  RESULT=$?
+done
