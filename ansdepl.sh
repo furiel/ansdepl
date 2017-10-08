563cdedac1ca08dea6883e02156f0446be2ea8c6
John Doe <johndoe@example.com>
2017-10-08 21:54:47 +0200

    Fixing loop error in ansdepl.sh

--- ansdepl.sh
+++ ansdepl.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+export GIT_TERMINAL_PROMPT=0
+
 REPO_PATH=https://github.com/furiel/ansdepl.git
 TMP_FILES=/var/pull
 
 ansible-pull -o -v -U "$REPO_PATH" -d "$TMP_FILES" -i "localhost," -c local ansdepl.yml
+RESULT=$?
 
-while [[ $RESULT -ne 0 ]] && [ $COUNTER -lt 10 ]; do
+while [[ $RESULT -ne 0 ]] && [[ $COUNTER -lt 10 ]]; do
   echo "+++Could not pull | $COUNTER. Try"
   COUNTER=$[COUNTER + 1]
   sleep 10
 
-  ansible-pull -p -U "$REPO_PATH" -d "$TMP_FILES" -i "localhost," -c local ansdepl.yml
+  ansible-pull -v -U "$REPO_PATH" -d "$TMP_FILES" -i "localhost," -c local ansdepl.yml
   RESULT=$?
 done
 
